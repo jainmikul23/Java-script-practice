6b53feaed66c1eb989ffb01b7815db3ef5a520ce
jainmikul23 <mikuljain09@gmail.com>
2025-11-12 11:23:54 +0000

    container config done

---
+++ basics_of_js/variables.js
@@ -0,0 +1,7 @@
+const accId = 123456;
+let accEmail = "mikuljain09@gmail.com";
+var accPassword = "Mikul@1234";
+accCity = "Pune";
+accId
+console.log(accId);
+console.table([accEmail,accPassword,accCity]);
